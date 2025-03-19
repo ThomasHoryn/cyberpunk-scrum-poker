887fb4938393e623fb9a82e855d2cabba502343a
Tomasz Horyn <tomasz.t.horyn@gmail.com>
2025-03-19 07:15:49 +0100

    Refactor HomePage component to remove terminal simulation and integrate Tailwind CSS styling; update App.svelte to eliminate TestTailwind component and enhance layout

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,15 +1,7 @@
 <script lang="ts">
   import HomePage from './lib/HomePage.svelte';
-  import TestTailwind from './lib/TestTailwind.svelte';
-  
-  // Set to true to test Tailwind
-  const showTest = false;
 </script>
 
 <main>
-  {#if showTest}
-    <TestTailwind />
-  {:else}
     <HomePage />
-  {/if}
 </main>
--- web/svelte.config.js
+++ web/svelte.config.js
@@ -4,4 +4,10 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  compilerOptions: {
+    runes: true,
+    compatibility: {
+      componentApi: 5 // Disables legacy component API
+    }
+  }
 }
